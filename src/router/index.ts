552dbfb50cd1b5fff32e8884259fea69f4f5fa61
ken iwata <ken.iwata@minkabu.co.jp>
2023-01-30 23:10:53 +0900

    お試しでnot found時のリダイレクト先を設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import MildDown from "../views/MilkdownView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/:pathMatch(.*)*", redirect: "/" },
     {
       path: "/",
       name: "home",
@@ -21,7 +22,6 @@ const router = createRouter({
       path: "/wiki",
       name: "wiki",
       component: () => import("../views/MilkdownView.vue"),
-      //component: MildDown,
     },
   ],
 });
