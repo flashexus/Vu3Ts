f3fbd1cc89892a0a2c37224adc5067cb9a6612d4
ken iwata <ken.iwata@minkabu.co.jp>
2023-02-21 09:12:03 +0900

    ✨ アイコンが表示されない問題を解決

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,17 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { fa } from "vuetify/iconsets/fa";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import { md } from "vuetify/iconsets/md";
+
+// Icon Sample
+// <v-icon icon="mdi-vuetify"></v-icon>
+// <v-icon icon="fa:fa-plus" />
+// <v-icon icon="md:home" />
 
 const vuetify = createVuetify({
   icons: {
@@ -19,6 +29,8 @@ const vuetify = createVuetify({
     aliases,
     sets: {
       mdi,
+      fa,
+      md,
     },
   },
   components,
